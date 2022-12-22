05ad58a264655643034533e0ece4bddd8dc6d5eb
PedroHSAlves <pedro.souza2@stellantis.com>
2022-12-22 14:51:15 -0300

    Finalizing the core code
    At this stage the code already works, there are some restrictions that will be fixed in the future.
    
    note: Add line name in SQL

--- SQL_manipulation.py
+++ SQL_manipulation.py
@@ -1,14 +1,39 @@
-def get_last_time(robot_name):
-    return "2020-12-15 12:57:13"
+import mysql.connector
+import authentication as auth
 
-def get_last_electrode_num(robot_name):
-    return 1
+class sql_manipulation():
+    def __init__(self) -> None:
+        try:
+            self._mydb = mysql.connector.connect(
+                host = "172.20.233.63",
+                user = auth._user,
+                password = auth._password,
+                database = "perfil"
+            )
+        except:
+            TypeError("database connection failure.")
+        
+        self._mycursor = self._mydb.cursor()
 
-def get_last_milling(robot_name):
-    return 50
+    def post_data(self, electrode_num: int, name_robot: str, n_points_applied: int, n_millins: int,electrode_no: int, time: str):
+        sql = "INSERT INTO wellding (electrode_num, name_robot, n_points_applied, n_millins,tool, time) VALUES (%s,%s,%s,%s,%s,%s)"
+        val = (electrode_num, name_robot, n_points_applied, n_millins,electrode_no, time)  
+        self._mycursor.execute(sql,val)
 
-def get_last_point_applied():
-    return
+        self._mydb.commit()
+        print(self._mycursor.rowcount, "record inserted.") 
+    
+    def get_last_time(self,robot_name):
+        return "2020-12-15 12:57:13"
 
-def revemove_line_in_SQL():
-    return
+    def get_last_electrode_num(self,robot_name):
+        return 1
+
+    def get_last_milling(self,robot_name):
+        return 50
+
+    def get_last_point_applied(self):
+        return
+
+    def revemove_line_in_SQL(self):
+        return
--- colector.py
+++ colector.py
@@ -2,13 +2,19 @@ import pandas as pd
 import numpy as np
 from paths import path
 import csv
-import SQL_manipulation as SQL
+from SQL_manipulation import sql_manipulation
 
 class protocol_current_values():
     def __init__(self):
         """
         """
-        self._df = pd.read_csv(path(file_name="Protocolo valores de corrente"), quoting=csv.QUOTE_NONE, sep = ";")
+        self._sql = sql_manipulation()
+
+        try:
+            self._df = pd.read_csv(path(file_name="Protocolo valores de corrente"), quoting=csv.QUOTE_NONE, sep = ";")
+        except:
+            TypeError("CSV reading failed")
+
         self.__data_formatting()
         self._filtered_df = self._df
 
@@ -26,9 +32,9 @@ class protocol_current_values():
         """
         """
         for robot_name in self._list_name:
-            last_date = SQL.get_last_time(robot_name)
-            self._last_electrode_num = SQL.get_last_electrode_num(robot_name)
-            self._last_milling = SQL.get_last_milling(robot_name)
+            last_date = self._sql.get_last_time(robot_name)
+            self._last_electrode_num = self._sql.get_last_electrode_num(robot_name) # fix this variable **** <---
+            self._last_milling_SQL = self._sql.get_last_milling(robot_name)
             
             #filters
             df_mask_date = self._df['dateTime'] > last_date
@@ -42,46 +48,79 @@ class protocol_current_values():
             #self._filtered_df.sort_values(by ='dateTime')
 
             print("\nrobot name: ", robot_name,end='\n\n')#Debug print
-            self.__check_electrode_change()
+            self.__check_electrode_change(robot_name)
             
             #print(self._df['timerName'].value_counts()[robot_name])
             #print(self._filtered_df.shape)
     
-    def __check_electrode_change(self):
+    def __check_electrode_change(self, robot_name):
         """
         """
+        electrode_no = self._filtered_df['electrodeNo'].unique()
+        len_electrode_no = len(electrode_no)
         list_index = self._filtered_df.index
-        tam_list_index = len(list_index)
+        len_index = len(list_index)
         count = 1 
-        points_applied = 0
-        n_milling = 0
+        points_applied = [0] * len_electrode_no
+        n_milling = [0] * len_electrode_no
+        change_clamp = False
 
-        for index in range(tam_list_index):
+        for index in range(len_index):
             # avoids looking in an index that does not exist. 
-            if (count + 1) > tam_list_index:
+            if (count + 1) > len_index:
                 break
             count += 1
 
-            if self._last_milling <= self._filtered_df['tipDressCounter'][self._filtered_df.index[0]] and index == 0:
-                SQL.revemove_line_in_SQL()
-                points_applied = SQL.get_last_point_applied
-            
-            if self._filtered_df['tipDressCounter'][list_index[index]] <= self._filtered_df['tipDressCounter'][list_index[index + 1]]:
-                points_applied += 1
-            else:
-                n_milling = self._filtered_df['tipDressCounter'][list_index[index]]
-                points_applied += 1
-                self._last_electrode_num += 1
-                #To MyphpAdmin
-                print("Points applieds: ", points_applied)#Debug print
 
-                points_applied = 0
+            electrode_no_index = np.where(electrode_no == self._filtered_df['electrodeNo'][self._filtered_df.index[index]])[0][0]
+
+            
+            #Check if you changed the clamp
+            if self._filtered_df['electrodeNo'][self._filtered_df.index[index]] != self._filtered_df['electrodeNo'][self._filtered_df.index[index + 1]]:
+                points_applied[electrode_no_index] += 1
+                n_milling[electrode_no_index] = self._filtered_df['tipDressCounter'][list_index[index]]
+
+                aux_index = np.where(electrode_no == self._filtered_df['electrodeNo'][self._filtered_df.index[index + 1]])[0][0]
+                if n_milling[aux_index] > self._filtered_df['tipDressCounter'][list_index[index + 1]] and points_applied[aux_index] != 0:
+                        points_applied[aux_index] += 1
+                        print("Condição de exeção")
+                        self._last_electrode_num += 1
+                        time = self._filtered_df['dateTime'][list_index[index]]
+                        
+                        self._sql.post_data(self._last_electrode_num,robot_name,points_applied[aux_index], int(n_milling[aux_index]),int(electrode_no[aux_index]),time)
+
+                        print("electrode No: ", electrode_no[aux_index ])#Debug print
+                        print("Points applieds: ", points_applied[aux_index ])#Debug print
+
+                        points_applied[aux_index] = 0
+                        
+                        print("next milling: ", self._filtered_df['tipDressCounter'][list_index[index + 1]])#Debug print
+                        print("milling: ", n_milling[aux_index])#Debug print
+                        print("electrode numer: ", self._last_electrode_num)#Debug print
+                        print("electrode replacement", end="\n\n")#Debug print
+                
+            
+            #Checks that you have applied a point
+            elif self._filtered_df['tipDressCounter'][list_index[index]] <= self._filtered_df['tipDressCounter'][list_index[index + 1]]:
+                points_applied[electrode_no_index] += 1
+
+            else:
+                n_milling[electrode_no_index] = self._filtered_df['tipDressCounter'][list_index[index]]
+                points_applied[electrode_no_index] += 1
+                self._last_electrode_num += 1
+                time = str(self._filtered_df['dateTime'][list_index[index]])
+                
+                self._sql.post_data(self._last_electrode_num,robot_name,points_applied[electrode_no_index],int(n_milling[electrode_no_index]),int(electrode_no[electrode_no_index]),time)
+
+                print("electrode No: ", electrode_no[electrode_no_index])
+                print("Points applieds: ", points_applied[electrode_no_index])#Debug print
+
+                points_applied[electrode_no_index] = 0
                 
                 print("next milling: ", self._filtered_df['tipDressCounter'][list_index[index + 1]])#Debug print
-                print("milling: ", n_milling)#Debug print
+                print("milling: ", n_milling[electrode_no_index])#Debug print
                 print("electrode numer: ", self._last_electrode_num)#Debug print
                 print("electrode replacement", end="\n\n")#Debug print
-
 
         
 
